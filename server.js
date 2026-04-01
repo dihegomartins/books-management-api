2ce5b4ca3344154ec163f206c9acd10b924362e7
Dihêgo Pires <dihegopires@gmail.com>
2026-04-01 13:26:25 -0300

    docs: add public API link to readme

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors"); // Subiu para o topo
 const livroRoutes = require("./src/routes/livroRoutes");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
-// Usando as rotas de livros
 app.use("/livros", livroRoutes);
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`🚀 Servidor Profissional rodando na porta ${PORT}`);
 });
